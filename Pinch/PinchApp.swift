331ccaec19869760247878637caa49d11ae46f06
Pierce Goulimis <piercemg2013@gmail.com>
2022-06-16 16:59:49 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Pierce Goulimis on 2022-06-16.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
