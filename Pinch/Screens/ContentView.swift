66ce9edfaf95abb5ce4e7c029f39f8f83cf950ea
Pierce Goulimis <piercemg2013@gmail.com>
2022-06-16 20:18:35 -0400

    Created double touch Gesture Recognizer

---
+++ Pinch/Screens/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Pierce Goulimis on 2022-06-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //MARK: - Properties
+    
+    //Store the state of the animation that shows the magazine cover onAppear (when app opens)
+    @State private var isAnimating: Bool = false
+    
+    
+    @State private var imageScale: CGFloat = 1
+    
+    
+    //MARK: - Body
+    
+    var body: some View {
+        NavigationView {
+            
+            ZStack {
+                
+                //MARK: - Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0).animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                
+                //MARK: - Tap Gesture
+                    //If the user taps on the Image 2 times
+                    .onTapGesture(count: 2) {
+                        
+                        //If the image is at original scale, magnify it!
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            //Image is already magnified, so zoom back out!
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                            
+                        }
+                        
+                    }
+                
+                
+                
+            } //End ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                isAnimating = true
+            }
+            
+        } //End NavigationView
+        .navigationViewStyle(.stack)
+        
+    }
+}
+
+//MARK: - Preview
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
