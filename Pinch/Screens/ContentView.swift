c5f1d5f3328af6899495625ddb26d00d6f3d5bef
Pierce Goulimis <piercemg2013@gmail.com>
2022-06-18 21:17:46 -0400

    Added Magnification Gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -99,9 +99,44 @@ struct ContentView: View {
                     
                     )
                 
+                //MARK: - Magnification
                 
-                
-                //MARK: - Long Press Gesture
+                    .gesture(
+                    
+                        MagnificationGesture()
+                            .onChanged({ value in
+                                
+                                withAnimation(.linear(duration: 1)) {
+                                    
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        
+                                        imageScale = value
+                                        
+                                    } else if imageScale > 5 {
+                                        
+                                        imageScale = 5
+                                        
+                                    }
+                                    
+                                }
+                                
+                            })
+                        
+                            .onEnded({ _ in
+                                
+                                if imageScale > 5 {
+                                    
+                                    imageScale = 5
+                                    
+                                } else if imageScale <= 1 {
+                                    
+                                    resetImageState()
+                                    
+                                }
+                                
+                            })
+                    
+                    )
                 
                 
                 
