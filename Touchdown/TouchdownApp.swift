9a6182bafde2b917794bef550ee465cbbd2a5ebb
Pierce Goulimis <piercemg2013@gmail.com>
2022-08-20 18:08:47 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Pierce Goulimis on 2022-08-20.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
